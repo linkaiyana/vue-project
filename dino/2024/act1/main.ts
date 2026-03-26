026c2a65a7697b6b21a7415010d619e6eb564a96
linkaiyan <1760942033@qq.com>
2026-03-26 14:13:08 +0800

    test: i18n报错问题

--- dino/2024/act1/main.ts
+++ dino/2024/act1/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: linkaiyan
  * @Date: 2025-12-03 15:00:50
- * @LastEditTime: 2026-03-26 11:24:50
+ * @LastEditTime: 2026-03-26 14:12:53
  * @LastEditors: linkaiyan
  * @Description:
  */
@@ -21,9 +21,9 @@ import 'virtual:uno.css' // 确保在重置样式之后引入 UnoCSS
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 app.use(creatI18n)
 app.use(createSentry)
+app.use(router)
 
 if (isDebug) {
   setTimeout(() => {
