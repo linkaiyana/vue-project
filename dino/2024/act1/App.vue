b11f3fcdf1482296f4b4c75241636babcc9ac506
linkaiyan <1760942033@qq.com>
2026-03-31 18:01:44 +0800

    feat: 测试更新

--- dino/2024/act1/App.vue
+++ dino/2024/act1/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: linkaiyan
  * @Date: 2025-12-03 15:00:50
- * @LastEditTime: 2026-03-26 14:41:05
+ * @LastEditTime: 2026-03-31 18:01:31
  * @LastEditors: linkaiyan
  * @Description:
 -->
@@ -12,7 +12,7 @@
   <ul>
     <li class="mb-5 mb10">
       1哈哈哈哈活动1
-      版本1.0.1
+      版本1.0.2
     </li>
     <li>2</li>
     <li>3</li>
