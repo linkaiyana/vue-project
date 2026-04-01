8e57a369a70408422c1fbaeaac70a73055440c2b
linkaiyan <1760942033@qq.com>
2026-04-01 16:25:10 +0800

    test: 测试缓存

--- dino/2024/act1/App.vue
+++ dino/2024/act1/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: linkaiyan
  * @Date: 2025-12-03 15:00:50
- * @LastEditTime: 2026-03-31 18:01:31
+ * @LastEditTime: 2026-04-01 16:24:57
  * @LastEditors: linkaiyan
  * @Description:
 -->
@@ -12,7 +12,7 @@
   <ul>
     <li class="mb-5 mb10">
       1哈哈哈哈活动1
-      版本1.0.2
+      版本1.0.3
     </li>
     <li>2</li>
     <li>3</li>
