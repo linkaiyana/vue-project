dd1491219409ff9c2735e9dd3e398e37ce26e907
linkaiyan <1760942033@qq.com>
2026-04-10 17:51:42 +0800

    test: 测试缓存

--- dino/2024/act1/pages/about.vue
+++ dino/2024/act1/pages/about.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: linkaiyan
  * @Date: 2025-12-02 15:34:56
- * @LastEditTime: 2026-04-09 14:39:45
+ * @LastEditTime: 2026-04-10 17:51:29
  * @LastEditors: linkaiyan
  * @Description:
 -->
@@ -9,7 +9,7 @@
   <main class="about-page">
     <section class="about-page__panel">
       <p class="about-page__eyebrow">
-        Reward Preview2
+        Reward Preview3
       </p>
       <RewardCard />
     </section>
