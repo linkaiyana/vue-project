2f745d75976ccb3dcf9366d1720396a6a217a0fa
linkaiyan <1760942033@qq.com>
2026-04-10 17:13:08 +0800

    feat: 修改

--- vite/build.ts
+++ vite/build.ts
@@ -1,13 +1,19 @@
 /*
  * @Author: linkaiyan
  * @Date: 2026-04-07 10:27:39
- * @LastEditTime: 2026-04-07 10:58:41
+ * @LastEditTime: 2026-04-09 15:33:30
  * @LastEditors: linkaiyan
  * @Description:
  */
 import type { BuildOptions } from 'vite'
 import type { SharedVendorRuntimeConfig } from './plugins/sharedVendor'
 import { resolve } from 'node:path'
+
+// unplugin-auto-import 和 external导致的警告，忽略
+function shouldIgnoreWarning(warning: { code?: string, message: string }) {
+  return warning.code === 'UNUSED_EXTERNAL_IMPORT'
+    && warning.message.includes('"resolveComponent" is imported from external module "vue" but never used')
+}
 
 function manualChunks(id: string) {
   if (!id.includes('node_modules'))
@@ -37,6 +43,12 @@ export default (
     rollupOptions: {
       input: resolve(customEntry, 'index.html'),
       external: sharedVendor.external,
+      onwarn(warning, warn) {
+        if (shouldIgnoreWarning(warning))
+          return
+
+        warn(warning)
+      },
       output: {
         manualChunks,
         chunkFileNames: 'static/js/[name]-[hash].js',
